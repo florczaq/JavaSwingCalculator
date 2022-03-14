d835be3b6fb89d92f21e63cdce8436929da74940
florczaq <mikiflor24@gmail.com>
2022-03-14 19:10:50 +0100

    a

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,14 +1,9 @@
 import javax.swing.JPanel;
 import java.awt.*;
-// import javax.swing.*;
 
 public class MyPanel extends JPanel{
     MyPanel(LayoutManager layout, Dimension dimension){
         setLayout(layout);
         setPreferredSize(dimension);
     }
-
-    // protected void addComponent(Component c){
-    //     super.add(c);
-    // }
 }
