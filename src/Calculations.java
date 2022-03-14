47b65179eef27e6285cd464d8de9632b11ff6907
florczaq <mikiflor24@gmail.com>
2022-03-14 22:30:05 +0100

    d

--- src/Calculations.java
+++ src/Calculations.java
@@ -3,9 +3,13 @@ import javax.script.ScriptException;
 import javax.script.ScriptEngine;
 
 public class Calculations {
-    public String calculate(String result) throws ScriptException{
-        ScriptEngineManager mgr = new ScriptEngineManager();
-        ScriptEngine engine = mgr.getEngineByName("JavaScript");
-        return engine.eval(result).toString();
+    public String calculate(String result) throws ScriptException {
+        try {
+            ScriptEngineManager mgr = new ScriptEngineManager();
+            ScriptEngine engine = mgr.getEngineByName("JavaScript");
+            return engine.eval(result).toString();
+        } catch (Exception e) {
+            return " ";
+        }
     }
 }
